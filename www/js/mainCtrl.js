d1780c64b640e39592099451885cada9e288f146
Noah Yarian <noah.yarian@gmail.com>
2015-07-08 12:49:40 -0500

    arithmetic working

--- www/js/mainCtrl.js
+++ www/js/mainCtrl.js
@@ -2,6 +2,79 @@ angular.module('starter', ['ionic'])
 
 .controller('MainCtrl', ["$scope", function($scope) {
 
-  $scope.message = "Dude";
+  $scope.result = "0";
+  $scope.secondOperandStarted = false;
+
+  $scope.firstOperand;
+  $scope.operator;
+  $scope.secondOperand;
+
+  $scope.digit = function(value) {
+    if ($scope.operator) {
+      if (!$scope.secondOperandStarted) {
+        if (value === ".") {
+          $scope.result = "0.";
+        } else {
+          $scope.result = value.toString();
+        }
+        $scope.secondOperandStarted = true;
+      } else {
+        if (value === ".") {
+          if ($scope.result.indexOf(".") === -1) {
+            $scope.result += value.toString();
+          }
+        } else {
+          if ($scope.result === "0") {
+            $scope.result = value.toString();
+          } else {
+            $scope.result += value.toString();
+          }
+        }
+      }
+    } else {
+      if (value === ".") {
+        if ($scope.result.indexOf(".") === -1) {
+          $scope.result += value.toString();
+        }
+      } else {
+        if ($scope.result === "0") {
+          $scope.result = value.toString();
+        } else {
+          $scope.result += value.toString();
+        }
+      }
+    }
+  }
+
+  $scope.arith = function(type) {
+    $scope.firstOperand = +$scope.result;
+    $scope.operator = type;
+  }
+
+  $scope.clear = function() {
+    $scope.result = "0";
+    $scope.secondOperandStarted = false;
+    $scope.operator = null;
+  }
+
+  $scope.equals = function() {
+    if ($scope.operator) {
+      $scope.secondOperand = +$scope.result;
+    }
+    switch ($scope.operator) {
+      case "add":
+        $scope.result = ($scope.firstOperand + $scope.secondOperand).toString();
+        break;
+      case "sub":
+        $scope.result = ($scope.firstOperand - $scope.secondOperand).toString();
+        break;
+      case "div":
+        $scope.result = ($scope.firstOperand / $scope.secondOperand).toString();
+        break;
+      case "mult":
+        $scope.result = ($scope.firstOperand * $scope.secondOperand).toString();
+        break;
+    }
+  }
 
 }]);
